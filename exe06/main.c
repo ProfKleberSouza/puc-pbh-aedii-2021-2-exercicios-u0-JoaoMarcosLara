ea9e11294e893616b43e226471fd5714c4dfa7d2
João  Marcos Lara Paula Rodrigues <89215665+JoaoMarcosLara@users.noreply.github.com>
2021-08-29 13:52:51 +0000

    exercicio 6

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+// \ deixei "\"," |" salvos pois meu pc nao tem barra invertida e tenho que usar alt+9+2 e buga o cod tudo :)//
+void triangulo(int lado1,int lado2,int lado3){
+  if(lado1+lado2 > lado3 && lado2+lado3 >lado1 && lado1+lado3>lado2 ){
+    if(lado1 == lado2 && lado2 == lado3){
+      printf("triangulo equilatero");
+    }else if(lado1 == lado2 ||lado2  == lado3 ||lado1 == lado3){
+      printf("triangulo isoceles");
+    }else{
+      printf("triangulo escaleno");
+    }
+  }else{
+    printf("os lados nao formam um triangulo");
+  }
+
+
+}
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
-   return 0;
+  int lado1;
+  int lado2;
+  int lado3;
+  scanf("%d",&lado1);
+  scanf("%d",&lado2);
+  scanf("%d",&lado3);
+  triangulo(lado1,lado2,lado3);
+  return 0;
 }
